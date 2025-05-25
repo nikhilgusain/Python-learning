f517ba6fbac97fc6d25e03903e030a2f97afb8c0
Nikhil Gusain <nikhilgusain771@gmail.com>
2025-05-25 20:18:00 +0530

    deleted some files

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-
-            print("There was type error")
-        except ZeroDivisionError:
-            print("zero div error")
-        except Exception:
-            print("there was some error")
